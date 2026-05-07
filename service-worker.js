61890ef2b6b3d4c710ed4f544a2a563087e1e542
LouisCol-Project <lo.colin@epmistes.net>
2026-05-07 10:15:35 +0200

    test installation téléphone

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "cis-tav-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./VSAV.html",
+  "./FPT.html",
+  "./CCF.html",
+  "./VSR.html",
+  "./VTU.html",
+  "./images/logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
